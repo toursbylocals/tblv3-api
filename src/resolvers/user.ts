11af1cf61daf76519e4559b5cba8110b58a4d9c3
Jenny Jiang <jjiang@toursbylocals.com>
2022-07-19 22:42:01 -0700

    feat(Filter): Language

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,10 +1,15 @@
 import { User, UserInput } from '../types'
 import { User as UserMongo } from '../models'
-import { IUserSchema } from 'src/types/mongoModels';
+import { IUserSchema } from 'src/types/mongoModels'
 
 export async function getUser(userInput: UserInput) {
-    const found = await UserMongo.findOne({ firstName: userInput.firstName });
-    
-    return found as IUserSchema;
+  const found = await UserMongo.findOne({ firstName: userInput.firstName })
+
+  return found as IUserSchema
 }
 
+export async function getLanguage() {
+  const found = await UserMongo.find({}).select((x) => x.languages)
+
+  return found
+}
